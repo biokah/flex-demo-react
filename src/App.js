397d37a92717453c80b71f547d305c40cea78d3f
Maria Anais Reyes <maria.anaisr@gmail.com>
2023-01-23 21:46:11 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const styles = {
+    "flexDirection": "row",
+    "justifyContent": "center",
+    "alignItems": "stretch"
+  }
+  const [values, setValues] = useState(styles)
+
+  const updateProperties = (e, property) => {
+    const newValues = {...values}
+    newValues[property] = e.target.value
+    setValues(newValues)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Flexbox magic</h1>
+      {JSON.stringify(values)}
+      <div className="demo-wrapper">
+          <div className="demo-container">
+              <div className="item one">one</div>
+              <div className="item two">two</div>
+              <div className="item three">three</div>
+              <div className="item four">four</div>
+              <div className="item five">five</div>
+              <div className="y axis">cross axis</div>
+              <div className="x axis">primary axis</div>
+          </div>
+      </div>
+      <article>
+        <span>Flex Direction:</span>
+        <select onChange={(e) => {updateProperties(e, "flex-direction")}}>
+          <option value="row">Row</option>
+          <option value="row-reverse">Row-reverse</option>
+          <option value="column">Column</option>
+          <option value="column-reverse">Column-reverse</option>
+        </select>
+      </article>
+      <article>
+      <span>Justify Content:</span>
+      <select onChange={(e) => {updateProperties(e, "justify-content")}}>
+        <option value="center">Center</option>
+        <option value="start">Start</option>
+        <option value="space-between">Space-Between</option>
+        <option value="space-around">Space-Around</option>
+        <option value="space-evenly">Space-Evenly</option>
+      </select>
+      </article>
+      <article>
+        <span>Align Items:</span>
+      <select onChange={(e) => {updateProperties(e, "align-items")}}>
+        <option value="stretch" >Stretch</option>
+        <option value="start">Start</option>
+        <option value="center">Center</option>
+        <option value="end">End</option>
+      </select>
+      </article>
+      
     </div>
   );
 }
